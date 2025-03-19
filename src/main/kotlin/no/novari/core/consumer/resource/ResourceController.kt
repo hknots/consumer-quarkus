3039457475ef53241de52a051e8a8144f6c7f722
Hknots <hknots99@gmail.com>
2025-03-19 20:40:02 +0100

    struct: add ResourceController base

---
+++ src/main/kotlin/no/novari/core/consumer/resource/ResourceController.kt
@@ -0,0 +1,11 @@
+package no.novari.core.consumer.resource
+
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.ws.rs.Path
+
+@Path("/{resource}")
+@ApplicationScoped
+class ResourceController {
+
+
+}
